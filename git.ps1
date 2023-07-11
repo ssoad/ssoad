7633640d38fe15800db4f55569a1f7c87deb0406
Sohanuzzaman Soad <mdsoad@gmail.com>
2023-07-11 17:34:52 +0600

    Update git.ps1

--- git.ps1
+++ git.ps1
@@ -2,3 +2,7 @@ echo "Updating Git Config"
 git config user.email "mdsoad@gmail.com"
 git config user.name "Sohanuzzaman Soad"
 echo "Done"
+
+<# $Script = Invoke-WebRequest 'http://example.com/powershell.ps1'
+$ScriptBlock = [Scriptblock]::Create($Script.Content)
+Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @('someargument')) #>
